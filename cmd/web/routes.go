6de4cb4b5d0a0ff849f028874c34d33ca2c3dd8c
sksahu2097 <sksahu2097@gmail.com>
2023-02-13 08:31:44 +0530

    Implemented the csrf token use and tried some more middleware for the request validation

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/sksahu2097/go-project/pkg/config"
 	"github.com/sksahu2097/go-project/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+	mux.Use(writeToConsole)
+	mux.Use(NoSurf)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/sksahu2097/go-project
 go 1.19
 
 require github.com/go-chi/chi v1.5.4
+
+require github.com/justinas/nosurf v1.1.1 // indirect

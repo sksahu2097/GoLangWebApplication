898cc49d9353ec18a90fae61d5a4935a5f57ee10
sksahu2097 <sksahu2097@gmail.com>
2023-02-13 08:03:20 +0530

    Implemented the chi router technique

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,14 +3,14 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
 	"github.com/sksahu2097/go-project/pkg/config"
 	"github.com/sksahu2097/go-project/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
